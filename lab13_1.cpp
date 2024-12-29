c58b19247bc77a7b9cdb6e01044fa42d0d12c2dd
wh0ohm <wararat.boonchieng@gmail.com>
2024-12-29 23:59:30 +0700

    done

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,29 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+void stat(const double a[] ,int n ,double b[]){
+    double max = a[0] , min = a[0];
+    double sum = 0 , sum2 = 0 , sumg = 1 , sumh = 0  ;
+    for(int i=0; i < n; i++){
+        sum = sum + a[i];
+        sum2 = sum2 + pow(a[i],2);
+        sumg = sumg*a[i];
+        sumh = sumh+(1/a[i]);
+    }
+
+    b[0] = sum/n; 
+    b[1] = sqrt((sum2/n)-pow(sum/n,2)) ;
+    double gm,hm;
+    gm = pow(sumg,1.0/n);
+    b[2] = gm;
+    hm = (n/sumh);
+    b[3] = hm;
+
+    for(int i=0; i<n ; i++){
+        if(a[i]>max) max = a[i] ;
+        if(a[i]<min) min = a[i] ;
+    }
+
+    b[4] = max;
+    b[5] = min;
+}
