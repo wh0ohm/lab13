24b28f7d36a14700aebcc547c462e67c8eab6757
wh0ohm <wararat.boonchieng@gmail.com>
2024-12-30 00:12:23 +0700

    donee

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,30 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool a[N][M]){
+cout << "------------------------------------------------------------------------" << endl ;
+    for(int i = 0; i<N; i++){
+        cout << "|" ;
+        for(int j = 0; j<M; j++){
+            if(a[i][j] == 0){
+                cout << " " ;
+            }
+            else if(a[i][j] == 1){
+                cout << "*" ;
+            }
+        }
+        cout << "|" << endl ;
+    }
+    cout << "------------------------------------------------------------------------\n" ;
+}
+
+void updateImage(bool a[N][M],int s, int x,int y){
+    for(int i = 0; i < N ; i++){
+        for(int j = 0; j < M ; j++){
+            if(sqrt(pow(i-x,2)+pow(j-y,2)) <= s-1){
+               a[i][j] = 1; 
+            }
+        }
+    }
+}
